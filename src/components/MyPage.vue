49f89b819b52f348bc4c2b3bd58959b440206851
eeny <59357089+eeny@users.noreply.github.com>
2022-02-27 23:50:04 +0900

    Composition API 로 팔로워 검색 기능

--- src/components/MyPage.vue
+++ src/components/MyPage.vue
@@ -1,7 +1,7 @@
 <template>
   <div style="padding: 10px">
     <h4>팔로워</h4>
-    <input placeholder="?" />
+    <input placeholder="?" @input="search($event.target.value)" />
     <div class="post-header" v-for="(a, i) in follower" :key="i">
       <div class="profile" :style="`background-image:url(${a.image})`"></div>
       <span class="profile-name">{{ a.name }}</span>
@@ -17,14 +17,23 @@ export default {
   name: "MyPage",
   setup() {
     let follower = ref([]);
+    let followerOrigin = ref([]);
 
     onMounted(() => {
       axios.get("/follower.json").then((a) => {
         follower.value = a.data;
+        followerOrigin.value = [...a.data];
       });
     });
 
-    return { follower };
+    function search(word) {
+      let newFollower = followerOrigin.value.filter((a) => {
+        return a.name.indexOf(word) != -1;
+      });
+      follower.value = [...newFollower];
+    }
+
+    return { follower, search };
   },
 };
 </script>
